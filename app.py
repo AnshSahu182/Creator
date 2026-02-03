cbfce6024b8d7aaeb814de9a390f5a66ef36ce99
AnshSahu182 <anshsahu182@gmail.com>
2026-02-03 12:39:20 +0530

    db health api

--- app.py
+++ app.py
@@ -72,9 +72,6 @@ app.register_blueprint(preregister_bp, url_prefix="/api")
 @app.route("/", methods=["GET"])
 def health_check():
     try:
-        # simple MongoDB ping
-        mongo.cx.admin.command("ping")
-
         return jsonify({
             "status": "ok",
             "app": "running",
@@ -89,6 +86,24 @@ def health_check():
             "error": str(e)
         }), 500
 
+@app.route("/db_health", methods=["GET"])
+def db_health_check():
+    try:
+        # simple MongoDB ping
+        mongo.cx.admin.command("ping")
+
+        return jsonify({
+            "status": "ok",
+            "database": "connected"
+        }), 200
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "database": "disconnected",
+            "error": str(e)
+        }), 500
+    
 @app.route("/api/auth/google", methods=["GET"])
 def google_auth():
     return google_signup()
